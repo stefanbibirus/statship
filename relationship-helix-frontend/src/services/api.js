b6c3ffb9c765b24a32e379aa3f513307a7a849ec
Stefan Bibirus <stefan11bibirus@gmail.com>
2025-04-18 15:46:42 +0300

    Linking

--- relationship-helix-frontend/src/services/api.js
+++ relationship-helix-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // URL de bază pentru API
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 // Instanță Axios configurată pentru API
 export const api = axios.create({
